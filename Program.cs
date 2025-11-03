f66fb791e8d686df07efae2f43c7077268f3b2a2
TheArtiist <szucsbence2003@gmail.com>
2025-11-03 23:47:40 +0100

    do-while action controller in the program.cs main

--- Program.cs
+++ Program.cs
@@ -12,10 +12,32 @@ namespace SmartLibrary
 
             repo.Loader($"C:\\Users\\TheArtist\\Desktop\\Programing\\VisualStudio\\SmartLibrary\\books.json");
 
-            foreach (var book in repo.GetAllBooks())
+
+            int action = 0;
+            Console.WriteLine("Select an action:");
+            do
             {
-                Console.WriteLine($"{book.title} - {book.author} - {book.pages} - {book.genre}");
-            }
+                
+                action = Convert.ToInt32(Console.ReadLine() ?? string.Empty);
+                
+                switch (action)
+                {
+                    case 1:
+                        var books = repo.GetAllBooks();
+                        foreach (var book in books)
+                        {
+                            Console.WriteLine($"{book.title} - {book.author} - {book.pages} - {book.genre}");
+                        }
+                        break;
+                    case 2:
+                        //ToDo
+                        break;
+                    default:
+                        action = -1;
+                        break;
+                }
+
+            } while (action == -1);
 
             //logger.writeToFile();
         }
